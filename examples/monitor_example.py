84e56d37ac0b2abe2eb33b219691b84290d24fc4
choijunDoo <wnsen1003@naver.com>
2022-03-15 11:17:46 +0900

    Gaze 추가

--- examples/monitor_example.py
+++ examples/monitor_example.py
@@ -4,16 +4,19 @@ import os
 from core.monitor import Monitor
 from parts.gsr import GSR
 from parts.eeg import EEG
+from parts.gaze import Gaze
 
 monitor = Monitor()
 
 gsr = GSR('CD36CD')
 eeg = EEG()
+gaze = Gaze()
 
 print("Instances are generated")
 
 monitor.add_part(eeg)
 monitor.add_part(gsr)
+monitor.add_part(gaze)
 
 print("parts are added")
 
